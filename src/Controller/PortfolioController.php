14b85a55269da1305b557bbfa8d8c5c05f2999ee
Noa <noa.tavernier@etu.univ-st-etienne.fr>
2025-04-07 18:01:20 +0200

    msj07042025

---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PortfolioController extends AbstractController{
+    #[Route('/portfolio', name: 'app_portfolio')]
+    public function index(): Response
+    {
+        return $this->render('portfolio/index.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    #[Route('/passion', name: 'app_passion')]
+    public function passion(): Response
+    {
+        return $this->render('portfolio/passion.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('portfolio/cv.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    #[Route('/formulaire', name: 'app_formulaire')]
+    public function formulaire(): Response
+    {
+        return $this->render('portfolio/formulaire.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    #[Route('/eportfolio', name: 'app_eportfolio')]
+    public function eportfolio(): Response
+    {
+        return $this->render('portfolio/eportfolio.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+}
